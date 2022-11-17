671ed7b0af3d36975a19fb99e47656725b4a2e54
mircearoata <mircearoatapalade@yahoo.ro>
2022-11-17 11:55:32 +0100

    Use HooksOnly stripped SML for updating the project

---
+++ SML_HooksOnly/SML/Source/SML/SML.Build.cs
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.IO;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using Tools.DotNETCommon;
+
+public class SML : ModuleRules
+{
+    public SML(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        bLegacyPublicIncludePaths = false;
+        
+        PublicDependencyModuleNames.AddRange(new[]
+        {
+            "Core", "CoreUObject"
+        });
+        
+        var thirdPartyFolder = Path.Combine(ModuleDirectory, "../../ThirdParty");
+        PublicIncludePaths.Add(Path.Combine(thirdPartyFolder, "include"));
+        
+        var platformName = Target.Platform.ToString();
+        var libraryFolder = Path.Combine(thirdPartyFolder, platformName);
+        
+        PublicAdditionalLibraries.Add(Path.Combine(libraryFolder, "funchook.lib"));
+        PublicAdditionalLibraries.Add(Path.Combine(libraryFolder, "AssemblyAnalyzer.lib"));
+        PublicAdditionalLibraries.Add(Path.Combine(libraryFolder, "Zydis.lib"));
+        PublicAdditionalLibraries.Add(Path.Combine(libraryFolder, "Zycore.lib"));
+    }
+}
